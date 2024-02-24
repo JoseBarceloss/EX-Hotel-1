75164d5bf7bae9a079c0cb26cc14ca8b289f0a0c
Barcelos <barcelos123@Ubuntu.myguest.virtualbox.org>
2024-02-24 00:11:10 -0300

    req 4

---
+++ src/04_challenge.sql
@@ -0,0 +1,9 @@
+INSERT INTO user (id, name, email, password, created_at, updated_at)
+VALUES ('Terry', 'Medhurst', 'atuny0@email.com', 31, '2022-12-31'),
+('Miles', 'Hills', 'rshawe2@dotmail.com', 17, '2023-05-10'),
+('Sheldon', 'Hills', 'num41@email.com', 18, '2022-12-25'),
+('Mavis', 'Abbott', 'kmeus4@email.com', 60, '2022-06-01'),
+('Oleta', 'Schultz', 'dpettegre6@email.com', 29, '2023-09-25'),
+('Ewell', 'Mueller', 'ggude7@dotmail.com', 15, '2019-09-02'),
+('Demetrius', 'Corkery', 'nloiterton8@email.com', 31, '2023-08-03'),
+('Eleanora', 'Price', 'umcgourty9@dotmail.com', 39, '2018-01-16');
